55bb783888caf09453d0eb638e798a592dd0e4b1
ogbaje emmanuel <emmogba@gmail.com>
2025-07-29 09:39:20 +0100

    edits4

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -110,6 +110,14 @@ export const Contact = () => {
                 </a>
               </div>
               <div className="flex items-center group">
+                <div className="w-12 h-12 bg-blue-500/10 rounded-lg flex items-center justify-center group-hover:bg-blue-500/20 transition-colors">
+                  <Computer  className="w-6 h-6 text-blue-400" />
+                </div>
+                <a href="mailto:contact@example.com" className="ml-4 text-gray-300 hover:text-blue-400 transition-colors">
+                https://www.linkedin.com/in/emmanuel-ogbaje-885738223
+                </a>
+              </div>
+              <div className="flex items-center group">
                 <div className="w-12 h-12 bg-purple-500/10 rounded-lg flex items-center justify-center group-hover:bg-purple-500/20 transition-colors">
                   <Phone className="w-6 h-6 text-purple-400" />
                 </div>
